6a1f9a0e9fd5fe30115b1b80603e1125a17cfede
root <lanremidelola@gmail.com>
2024-01-11 23:12:09 +0000

    upload files

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,112 +3,128 @@
    Description: The N queens puzzle is the challenge of placing N non-attacking
                 queens on an N×N chessboard. Write a program that solves the N
                 queens problem.
-   Usage: nqueens N
+   Usage: nqueens N:
+          If the user called the program with the wrong number of arguments,
+          print Usage: nqueens N, followed by a new line, and exit with the
+          status 1
+   where N must be an integer greater or equal to 4:
+          If N is not an integer, print N must be a number, followed by a new
+          line, and exit with the status 1
+          If N is smaller than 4, print N must be at least 4, followed by a new
+          line, and exit with the status 1
+   The program should print every possible solution to the problem:
+          One solution per line
+          Format: see example
+          You don’t have to print the solutions in a specific order
+   You are only allowed to import the sys module
 """
+
 
 import sys
 
 
 def print_board(board):
-    """Prints the positions of queens on the chessboard.
-
+    """ print_board
     Args:
-        board (list): A list of lists representing the chessboard.
+        board - list of list with length sys.argv[1]
     """
-    queen_positions = []
-    for a, row in enumerate(board):
-        for b, col in enumerate(row):
+    new_list = []
+    for i, row in enumerate(board):
+        value = []
+        for j, col in enumerate(row):
             if col == 1:
-                queen_positions.append([a, b])
-    print(queen_positions)
+                value.append(i)
+                value.append(j)
+        new_list.append(value)
+
+    print(new_list)
 
 
 def isSafe(board, row, col, number):
-    """Checks if placing a queen at a specific position is safe.
-
+    """ isSafe
     Args:
-        board (list): A list of lists representing the chessboard.
-        row (int): Row index to check for safety.
-        col (int): Column index to check for safety.
-        number (int): Size of the chessboard.
-
-    Returns:
-        bool: True if placing a queen is safe, False otherwise.
+        board - list of list with length sys.argv[1]
+        row - row to check if is safe doing a movement in this position
+        col - col to check if is safe doing a movement in this position
+        number: size of the board
+    Return: True of False
     """
+
     # Check this row in the left side
-    for a in range(col):
-        if board[row][a] == 1:
+    for i in range(col):
+        if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on the left side
-    for a, b in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[a][b] == 1:
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on the left side
-    for a, b in zip(range(row, number, 1), range(col, -1, -1)):
-        if board[a][b] == 1:
+    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
 
     return True
 
 
 def solveNQUtil(board, col, number):
-    """Recursively finds all possible solutions for N Queens.
-
+    """ Auxiliar method to find the posibilities of answer
     Args:
-        board (list): A list of lists representing the chessboard.
-        col (int): Current column being considered.
-        number (int): Size of the chessboard.
-
+        board - Board to resolve
+        col - Number of col
+        number - size of the board
     Returns:
-        bool: True if a solution is found, False otherwise.
+        All the posibilites to solve the problem
     """
-    if col == number:
+
+    if (col == number):
         print_board(board)
         return True
     res = False
-    for a in range(number):
-        if isSafe(board, a, col, number):
-            board[a][col] = 1
+    for i in range(number):
+
+        if (isSafe(board, i, col, number)):
+
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Make result true if any placement
+            # is possible
             res = solveNQUtil(board, col + 1, number) or res
-            board[a][col] = 0  # BACKTRACK
+
+            board[i][col] = 0  # BACKTRACK
 
     return res
 
 
 def solve(number):
-    """Finds and prints all possible solutions for N Queens.
-
+    """ Find all the posibilities if exists
     Args:
-        number (int): Size of the chessboard.
+        number - size of the board
     """
-    board = [[0 for a in range(number)] for a in range(number)]
+    board = [[0 for i in range(number)]for i in range(number)]
 
     if not solveNQUtil(board, 0, number):
-        print("No solution exists.")
+        return False
+
+    return True
 
 
 def validate(args):
-    """Validates the input data and returns the size of the chessboard.
-
+    """ Validate the input data to verify if the size to
+        answer is posible
     Args:
-        args (list): List of command line arguments.
-
-    Returns:
-        int: Size of the chessboard.
-
-    Raises:
-        ValueError: If the input is invalid.
+        args - sys.argv
     """
-    if len(args) == 2:
+    if (len(args) == 2):
+        # Validate data
         try:
             number = int(args[1])
-        except ValueError:
-            print("N must be a number.")
+        except Exception:
+            print("N must be a number")
             exit(1)
         if number < 4:
-            print("N must be at least 4.")
+            print("N must be at least 4")
             exit(1)
         return number
     else:
@@ -117,6 +133,8 @@ def validate(args):
 
 
 if __name__ == "__main__":
-    """Main method to execute the application."""
+    """ Main method to execute the application
+    """
+
     number = validate(sys.argv)
     solve(number)
