3af211bbf4109f23d6500c26b75083e759fe4adb
root <lanremidelola@gmail.com>
2024-01-11 23:16:58 +0000

    upload files

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,113 +1,122 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
 """
+   Description: The N queens puzzle is the challenge of placing N non-attacking
+                queens on an N×N chessboard. Write a program that solves the N
+                queens problem.
+   Usage: nqueens N
+"""
+
 import sys
 
 
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-pos = None
-"""The list of possible positions on the chessboard.
-"""
-
-
-def get_input():
-    """Retrieves and validates this program's argument.
-
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
-    n = 0
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
-
-
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
+def print_board(board):
+    """Prints the positions of queens on the chessboard.
 
     Args:
-        pos0 (list or tuple): The first queen's position.
-        pos1 (list or tuple): The second queen's position.
+        board (list): A list of lists representing the chessboard.
+    """
+    queen_positions = []
+    for a, row in enumerate(board):
+        for b, col in enumerate(row):
+            if col == 1:
+                queen_positions.append([a, b])
+    print(queen_positions)
+
+
+def isSafe(board, row, col, number):
+    """Checks if placing a queen at a specific position is safe.
+
+    Args:
+        board (list): A list of lists representing the chessboard.
+        row (int): Row index to check for safety.
+        col (int): Column index to check for safety.
+        number (int): Size of the chessboard.
 
     Returns:
-        bool: True if the queens are in an attacking position else False.
+        bool: True if placing a queen is safe, False otherwise.
     """
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
+    # Check this row in the left side
+    for a in range(col):
+        if board[row][a] == 1:
+            return False
+
+    # Check upper diagonal on the left side
+    for a, b in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[a][b] == 1:
+            return False
+
+    # Check lower diagonal on the left side
+    for a, b in zip(range(row, number, 1), range(col, -1, -1)):
+        if board[a][b] == 1:
+            return False
+
+    return True
+
+
+def solveNQUtil(board, col, number):
+    """Recursively finds all possible solutions for N Queens.
+
+    Args:
+        board (list): A list of lists representing the chessboard.
+        col (int): Current column being considered.
+        number (int): Size of the chessboard.
+
+    Returns:
+        bool: True if a solution is found, False otherwise.
+    """
+    if col == number:
+        print_board(board)
         return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+    res = False
+    for a in range(number):
+        if isSafe(board, a, col, number):
+            board[a][col] = 1
+            res = solveNQUtil(board, col + 1, number) or res
+            board[a][col] = 0  # BACKTRACK
+
+    return res
 
 
-def group_exists(group):
-    """Checks if a group exists in the list of solutions.
+def solve(number):
+    """Finds and prints all possible solutions for N Queens.
 
     Args:
-        group (list of integers): A group of possible positions.
+        number (int): Size of the chessboard.
+    """
+    board = [[0 for a in range(number)] for a in range(number)]
+
+    if not solveNQUtil(board, 0, number):
+        print("No solution exists.")
+
+
+def validate(args):
+    """Validates the input data and returns the size of the chessboard.
+
+    Args:
+        args (list): List of command line arguments.
 
     Returns:
-        bool: True if it exists, otherwise False.
+        int: Size of the chessboard.
+
+    Raises:
+        ValueError: If the input is invalid.
     """
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem.
-
-    Args:
-        row (int): The current row in the chessboard.
-        group (list of lists of integers): The group of valid positions.
-    """
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
+    if len(args) == 2:
+        try:
+            number = int(args[1])
+        except ValueError:
+            print("N must be a number.")
+            exit(1)
+        if number < 4:
+            print("N must be at least 4.")
+            exit(1)
+        return number
     else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
+        print("Usage: nqueens N")
+        exit(1)
 
 
-def get_solutions():
-    """Gets the solutions for the given chessboard size.
-    """
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+if __name__ == "__main__":
+    """Main method to execute the application."""
+    number = validate(sys.argv)
+    solve(number)
